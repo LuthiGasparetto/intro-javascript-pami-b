968d279eefaf77a25d46cc9267209a40dda72a04
LuthiGasparetto <125319070+LuthiGasparetto@users.noreply.github.com>
2023-02-13 22:49:42 -0300

    Aula Introdução
    Criação de variáveis com cautenação

---
+++ Exemplosjs/001-helloWorld.js
@@ -0,0 +1,37 @@
+
+//comentario de uma linha
+console.log('Estou desenvolvendo em java-script');
+
+/*
+Comentario de mais de
+uma linha
+Existe tres formas de declarar uma varialvel em java-script
+(var, let e const)
+*/
+
+let nome = 'Luthi';
+let idade = 27;
+let trabalha = true;
+let altura_pessoa = 1.88; //numero entre aspas é string
+let cores_favoritas = ['preto','branco', 'vermelho', 'azul']; // array
+let endereco = {
+    logadouro : 'Rua',
+    nmLog : 'Sao Sebastiao',
+    num : '164',
+    bairro : 'Pq. Universitario'
+}; //objeto
+
+let covid = null;
+let dengue;
+
+console.log(altura_pessoa);
+console.log('Nome: ' + nome);
+console.log('O ' + nome + ' tem ' + idade + ' anos.');
+console.log('');;
+console.log('---------------------------');
+console.log(typeof(dengue));
+
+
+
+
+
